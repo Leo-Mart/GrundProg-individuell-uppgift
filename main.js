f9489b03cb8729314b64782b5220b7ea7faa4dd0
Martin Leo <martin.christoffer.leo@gmail.com>
2023-11-01 20:07:17 +0100

    lade till lite kod för att hantera reaktioner på poster, just nu visar varje post bara det antal som finnns inbakat i dummjson-datan och skapa en egen post är hårdkodat till 0, så nästa steg är att lägga till ett sätt att gilla en post eller något i den stilen

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ btn = document.getElementsByClassName("getPostBtn");
 btn[0].addEventListener("click", getPost); */
 
 function getPost() {
-  fetch("https://dummyjson.com/posts/")
+  fetch("https://dummyjson.com/posts")
     .then((res) => res.json())
     .then((post) => {
       let mainContainer = document.getElementsByClassName("main-content");
@@ -38,12 +38,14 @@ function getPost() {
         let postTitle = document.createElement("h3");
         let postText = document.createElement("p");
         let postTags = document.createElement("span");
+        let postReactions = document.createElement("span");
 
         postTitle.innerText = post.posts[i].title;
         postText.innerText = post.posts[i].body;
         postTags.innerText = post.posts[i].tags;
+        postReactions.innerText = post.posts[i].reactions;
 
-        postContainer.append(postTitle, postTags, postText);
+        postContainer.append(postTitle, postTags, postReactions, postText );
         mainContainer[0].append(postContainer);
       }
     });
@@ -53,6 +55,7 @@ getPost();
 
 let postContainer = document.createElement("article");
 let mainContainer = document.getElementsByClassName("main-content");
+
 // Modal för create post delen
 let modal = document.getElementsByClassName("modal")[0];
 let btnOpenModal = document.getElementsByClassName("btn-open-modal")[0];
@@ -81,13 +84,15 @@ function createPost() {
     let postTitle = document.createElement("h3")
     let postText = document.createElement("p")
     let postTags = document.createElement("span")
+    let postReactions = document.createElement("span");
 
     
     postTitle.innerText = inputTitle.value;
     postText.innerText = inputText.value;
     postTags.innerText = inputTags.value;
-    console.log(postTitle);
-    postContainer.append(postTitle, postTags, postText);
+    postReactions = 0;
+    
+    postContainer.append(postTitle, postTags, postReactions, postText);
     mainContainer[0].append(postContainer);
 
     closeModal();
@@ -96,5 +101,17 @@ function createPost() {
 
 // hämta create post knappen och kör createPost() funktionen på denna
 let btnCreatePost = document.getElementsByClassName("btn-create-post");
-console.log(btnCreatePost[0]);
+
 btnCreatePost[0].addEventListener("click", createPost);
+
+// rent funktionellt tror jag sidan är mer eller mindre färdig? åtminstone om jag förstår beskrivningen rätt. 
+// för VG måste jag lägga in likes/reactions. Har sett att det finns en sådan property inbakad i datan från dummyJSON
+// antar då att det handlar om att visa den och om man då trcker på en "like" knapp ska denna gå upp med ett.
+// typ likes = post.posts.reactions sedan likes++; tror detta borde vara möjligt redan, men kan hända att det är pilligare än jag tror
+
+// att få dit reactions var ju inte så svårt, inte riktigt samma kanske på att göra egna poster, dock borde jag kunna köra något liknande relativt enkelt
+
+// local storage delen av VG kravet är svårare, vi har ju inte alls gått igenom detta ännu. Så tänker jag fokuserar på likes/reactions
+// och så kan jag förstås börja kolla på local storage redan nu/snart
+
+// passande commits har jag försökt göra från början så förhoppningsvis är det steget klart av VG kraven
