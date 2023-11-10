bc912fc23144d3f2cd2c0001042ab238c32bcc42
Martin Leo <martin.christoffer.leo@gmail.com>
2023-11-10 15:17:42 +0100

    flyttade runt lite i koden och städade upp lite, började också ge mig på att få reactions att funka. Har lyckats lägga in en knapp men vet inte riktigt hur jag skall gå vidare, verkar som om jag har lite problem med att komma åt de dynamiskt skapade knapparna. Fick inte det till att funka :<

--- main.js
+++ main.js
@@ -26,85 +26,97 @@
 btn = document.getElementsByClassName("getPostBtn");
 
 btn[0].addEventListener("click", getPost); */
-
-function getPost() {
-  fetch("https://dummyjson.com/posts")
-    .then((res) => res.json())
-    .then((post) => {
-      let mainContainer = document.getElementsByClassName("main-content");
-      for (let i = 0; i < post.posts.length; i++) {
-        let postContainer = document.createElement("article");
-
-        let postTitle = document.createElement("h3");
-        let postText = document.createElement("p");
-        let postTags = document.createElement("span");
-        let postReactions = document.createElement("span");
-
-        postTitle.innerText = post.posts[i].title;
-        postText.innerText = post.posts[i].body;
-        postTags.innerText = post.posts[i].tags;
-        postReactions.innerText = post.posts[i].reactions;
-
-        postContainer.append(postTitle, postTags, postReactions, postText );
-        mainContainer[0].append(postContainer);
-      }
-    });
-}
-
-getPost();
-
-let postContainer = document.createElement("article");
+// ****** DECLARATIONS *******
+// declaring these in the global scope so they are accessable by everthying that want to create new posts or need to use them to manipulate posts, I think there is a better way to do this but not sure how
 let mainContainer = document.getElementsByClassName("main-content");
-
-// Modal för create post delen
 let modal = document.getElementsByClassName("modal")[0];
 let btnOpenModal = document.getElementsByClassName("btn-open-modal")[0];
 let btnCloseModal = document.getElementsByClassName("close")[0];
+let btnCreatePost = document.getElementsByClassName("btn-create-post");
+let likeBtn = document.createElement("button");
+let postReactions = document.createElement("span");
 
+// ****** INITAL POST-FETCHING *******
+// fetches all the post from dummyjson and dynamically creates new elements in which to display them
+fetch("https://dummyjson.com/posts")
+  .then((res) => res.json())
+  .then((post) => {
+    for (let i = 0; i < post.posts.length; i++) {
+      // creating the elements for the content dynamically
+      let postContainer = document.createElement("article");
+      postContainer.classList.add("post-container");
+      let postTitle = document.createElement("h3");
+      postTitle.classList.add("post-title");
+      let postText = document.createElement("p");
+      postText.classList.add("post-text");
+      let postTags = document.createElement("span");
+      postTags.classList.add("post-tags");
+      postReactions = document.createElement("span");
+      postReactions.classList.add("post-reactions");
+      likeBtn = document.createElement("button");
+      likeBtn.classList.add("btn");
+      likeBtn.classList.add("like-btn")
+      let postLikeIcon = document.createElement("i");
+      postLikeIcon.classList.add("fa-regular");
+      postLikeIcon.classList.add("fa-heart");
+      likeBtn.append(postLikeIcon);
+
+
+      // putting the correct data into the correct element
+      postTitle.innerText = post.posts[i].title;
+      postText.innerText = post.posts[i].body;
+      postTags.innerText = post.posts[i].tags;
+      postReactions.innerText = post.posts[i].reactions;
+      // appends the title, tags, reactions and actual text to its own container that is then appended to the main container for the posts
+      postContainer.append(postTitle, postTags, postReactions, likeBtn, postText);
+      mainContainer[0].append(postContainer);
+      
+    }
+  });
+// ****** EVENTS *******
+// opens the modal
+btnOpenModal.addEventListener("click", openModal);
+// closes the modal
+btnCloseModal.addEventListener("click", closeModal);
+//creates a new post using the createPost function
+btnCreatePost[0].addEventListener("click", createPost);
+
+let likeBtns = document.querySelectorAll(".btn")
+console.log(likeBtns);
+
+// ****** FUNCTIONS *******
+// might be a bit overkill to make these functions, but makes them easier to reuse down the line I suppose
 function openModal() {
-    modal.classList.remove("hidden")
+  modal.classList.remove("hidden");
 }
 
 function closeModal() {
-    modal.classList.add("hidden")
+  modal.classList.add("hidden");
 }
 
-btnOpenModal.addEventListener("click", openModal);
-
-btnCloseModal.addEventListener("click", closeModal);
-
-// ta info från input fält i modal och skapa en post med denna info sedan append till main
-
+// reads info from the input fields in the modal and then creates a new post and adds it to the end of the other posts.
 function createPost() {
+  let inputTitle = document.getElementById("post-title");
+  let inputTags = document.getElementById("post-tags");
+  let inputText = document.getElementById("post-text");
 
-    let inputTitle = document.getElementById("post-title");
-    let inputTags = document.getElementById("post-tags");
-    let inputText = document.getElementById("post-text");
+  let postTitle = document.createElement("h3");
+  let postText = document.createElement("p");
+  let postTags = document.createElement("span");
+  let postReactions = document.createElement("span");
 
-    let postTitle = document.createElement("h3")
-    let postText = document.createElement("p")
-    let postTags = document.createElement("span")
-    let postReactions = document.createElement("span");
+  postTitle.innerText = inputTitle.value;
+  postText.innerText = inputText.value;
+  postTags.innerText = inputTags.value;
+  postReactions = 0;
 
-    
-    postTitle.innerText = inputTitle.value;
-    postText.innerText = inputText.value;
-    postTags.innerText = inputTags.value;
-    postReactions = 0;
-    
-    postContainer.append(postTitle, postTags, postReactions, postText);
-    mainContainer[0].append(postContainer);
+  postContainer.append(postTitle, postTags, postReactions, postText);
+  mainContainer[0].append(postContainer);
 
-    closeModal();
-
+  closeModal();
 }
 
-// hämta create post knappen och kör createPost() funktionen på denna
-let btnCreatePost = document.getElementsByClassName("btn-create-post");
-
-btnCreatePost[0].addEventListener("click", createPost);
-
-// rent funktionellt tror jag sidan är mer eller mindre färdig? åtminstone om jag förstår beskrivningen rätt. 
+// rent funktionellt tror jag sidan är mer eller mindre färdig? åtminstone om jag förstår beskrivningen rätt.
 // för VG måste jag lägga in likes/reactions. Har sett att det finns en sådan property inbakad i datan från dummyJSON
 // antar då att det handlar om att visa den och om man då trcker på en "like" knapp ska denna gå upp med ett.
 // typ likes = post.posts.reactions sedan likes++; tror detta borde vara möjligt redan, men kan hända att det är pilligare än jag tror
@@ -113,5 +125,8 @@ btnCreatePost[0].addEventListener("click", createPost);
 
 // local storage delen av VG kravet är svårare, vi har ju inte alls gått igenom detta ännu. Så tänker jag fokuserar på likes/reactions
 // och så kan jag förstås börja kolla på local storage redan nu/snart
+// så local storage kan bara lagra strings. Därmed måste jag konvertera det jag vill ha i localstorage till en string genom json.stringify för att sedan konvertera från string tillbaka till object eller liknande så använder jag json.parse
+// sedan är det localstorage.setitem för att lagra
+// localstorage.getitem för att hämta.
 
 // passande commits har jag försökt göra från början så förhoppningsvis är det steget klart av VG kraven
