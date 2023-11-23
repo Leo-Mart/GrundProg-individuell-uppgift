3686564a23f6072b71140257e6de7b5386bbf3fd
Martin Leo <martin.christoffer.leo@gmail.com>
2023-11-23 18:10:22 +0100

    added in the functionality to updated the stored valued for reactions with the new one as the user clicks the like button. I believe that is all the functionality required for the assignment, so all that is left is some styling. Cleared out some old comments and added a few new ones that attemps to explain the newly added stuff.

--- main.js
+++ main.js
@@ -19,7 +19,7 @@ let postStorage = [];
 // this bit fetches the data from localStorage is there is anything otherwise it fetches the data fom dummyjson and dynamically creates the posts on the page.
 if (JSON.parse(localStorage.getItem("posts"))) {
   getLocalData();
-} else { 
+} else {
   getRemoteData();
 }
 // ******* END OF INITAL POST FETCHING********
@@ -35,7 +35,7 @@ btnCreatePost[0].addEventListener("click", createPost);
 // ******* END OF EVENTS*******
 
 // ****** FUNCTIONS *******
-// open and close modal functions are only for adding or removeing the class from the modal to show/hide it. Might just bake these into the function/eventlistener above 
+// open and close modal functions are only for adding or removeing the class from the modal to show/hide it. Might just bake these into the function/eventlistener above
 function openModal() {
   modal.classList.remove("hidden");
 }
@@ -45,18 +45,24 @@ function closeModal() {
 }
 
 function likes(e) {
+  // Shoutout ot Jonh Smilgas project tutorials for going through this type of selection.
   let element = e.currentTarget.parentElement.querySelector(".post-reactions");
   counter =
     e.currentTarget.parentElement.querySelector(".post-reactions").innerText;
+  // selecting the parent element (post container) of the parent element(reaction container) to fetch the id from the data-id attribute.
+  let idSelect = e.currentTarget.parentElement.parentElement.dataset.id;
   counter++;
   element.innerText = counter;
-} // I think this works? huge shoutout to john smilgas project tutorials.
-//  Now there is no cap so the user can just give a post likes to their hearts content and the same user can like however many times they want too, but still.
-
+  // runs the editLikes function to update the value of reactions in the localstorage with the new value from counter after the user clicks the like button
+  editLikes(idSelect, counter);
+} 
 // reads info from the input fields in the modal and then creates a new post and adds it to the end of the other posts.
 function createPost() {
   postContainer = document.createElement("article");
   postContainer.classList.add("post-container");
+  // adding this to create an id for the new post that the user creates so I can use the ID later to select the correct post and update it's reactions value in the localstorage
+  let attr = Date.now();
+  postContainer.setAttribute("data-id", attr);
 
   let inputTitle = document.getElementById("post-title");
   let inputTags = document.getElementById("post-tags");
@@ -82,12 +88,7 @@ function createPost() {
   likeBtn.classList.add("btn");
   likeBtn.classList.add("like-btn");
 
-  let postLikeIcon = document.createElement("i"); // this doesnt work on the post that the user can create by themselves,
-  // for some reason it selects only the main ones in the fetch
-  // my assumption is that it still points to the last created post during the fetch and doesnt change that when a user creates a new post
-  // maybe because the fetched posts are created on load, but the create post button doesnt create a new post untill the user clicks it, for some reason it doesn't point to the user generated post after this?
-  // think I found the issue, the newly created post was not created properly, it was getting created into the same container as the last fetched post so it naturally pointed towards the containers button
-  // however now when I click the button it gives me an error main.js:83 Uncaught TypeError: Cannot read properties of null (reading 'innerText') but this was solved by a fixing a misstake on line 129 where I had put the value of postReactions to 0 instead of just the innertext
+  let postLikeIcon = document.createElement("i");
   postLikeIcon.classList.add("fa-regular");
   postLikeIcon.classList.add("fa-heart");
   likeBtn.append(postLikeIcon);
@@ -95,6 +96,7 @@ function createPost() {
 
   // creates a object with the post info for storing in the localstorage
   const userPost = {
+    id: attr,
     title: inputTitle.value,
     body: inputText.value,
     tags: inputTags.value,
@@ -118,7 +120,6 @@ function createPost() {
 
 // fetches the data from dummyjson and dynamically creates post with it
 function getRemoteData() {
-
   // fetches all the post from dummyjson and dynamically creates new elements in which to display them
   // only pulls 30 posts but that seems to be by default, as I understand it I can fetch all the posts by putting limit=0 in the fetch url
   // this wont run if there already is some data in the localstorage
@@ -129,6 +130,10 @@ function getRemoteData() {
       for (let i = 0; i < post.posts.length; i++) {
         // creating the elements for the content dynamically
         postContainer = document.createElement("article");
+        // assigns the ID of the post to the data-id attribute to select the post later on so I can update the local storage
+        let attr = post.posts[i].id;
+        postContainer.setAttribute("data-id", attr);
+
         postContainer.classList.add("post-container");
 
         let postTitle = document.createElement("h3");
@@ -178,6 +183,9 @@ function getLocalData() {
     // creating the elements for the content dynamically
     postContainer = document.createElement("article");
     postContainer.classList.add("post-container");
+    // assigns the ID of the post to the data-id attribute to select the post later on so I can update the local storage
+    let attr = postStorage[i].id;
+    postContainer.setAttribute("data-id", attr);
 
     let postTitle = document.createElement("h3");
     postTitle.classList.add("post-title");
@@ -205,9 +213,6 @@ function getLocalData() {
     likeBtn.append(postLikeIcon);
     reactionContainer.append(postReactions, likeBtn);
 
-    /* // store the post in the postStorage array and then stores that in the local storage
-      postStorage.push(post.posts[i]);
-      localStorage.setItem("posts", JSON.stringify(postStorage)); */
     // putting the correct data into the correct element
     postTitle.innerText = postStorage[i].title;
     postText.innerText = postStorage[i].body;
@@ -219,7 +224,21 @@ function getLocalData() {
   }
 }
 
+function editLikes(id, value) {
+  // fetch the localstorage data again to find the item that I want to edit.
+  let items = JSON.parse(localStorage.getItem("posts"));
+  // loop through the items (fetched from the localstorage) and find the item with the corresponding ID and update that objects reactions value with the value of counter.
+  items = items.map(function (item) {
+    // took me awhile to figure this out, had to add the parseint because it was trying to compare a string to a number, just having == might've worked as well?
+    if (item.id === parseInt(id)) {
+      item.reactions = value;
+    }
 
+    console.log(item);
+    return item;
+  });
+  localStorage.setItem("posts", JSON.stringify(items));
+}
 // ***** TODO *******
 // so I think I'm almost done with the functionallity? Main thing left as far as I know is that it's currently just saving the initial value of reactions not the updated on. So i'll have to save that value somewhere. possibly in the likes function.
 // my current theory here is that I can possibly use the ID key that is contained in the object by default from dummyjson to make sure I select the correct post then update the saved value for reactions in the localstorage.
